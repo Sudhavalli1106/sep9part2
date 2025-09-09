241f475ccb4983009f701ddd05fae27a7148412a
Sudhavalli1106 <sudhavalli.parthasarathy@matrimony.com>
2025-09-09 11:53:34 +0530

    Update file2.js

--- file2.js
+++ file2.js
@@ -1,6 +1,6 @@
 console.log("2nd file")
 let a="40"
-let b=20
+let b=40
 console.log("test rebase - local - modified by remote");
-console.log("new line added");
+console.log("new line added")4
 console.log("test rebase remote");
