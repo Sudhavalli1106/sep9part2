6942bf397062a815fe6bc53ab4df87a55e0ec436
sudhavalli <sudhavalli.parthasarathy@matrimony.com>
2025-09-09 11:57:12 +0530

    check normal pull

--- file2.js
+++ file2.js
@@ -1,8 +1,9 @@
 console.log("2nd file")
 let a="40"
-let b=90
+let b=70
 console.log("test rebase - local - modified by remote");
 console.log("new line added")4
 console.log("test rebase remote");
 
 let x=7;
+let y=9
